48c0e860dc56f4cd1d1b4c7ed57060471894b197
Chinmay <chinmay.cc.06@gmail.com>
2022-10-09 10:23:12 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -218,6 +218,24 @@ int main()
     Sudoku sudoku;
     sudoku.initialize(); 
 
+    
+    vector<vector<int>>grid = sudoku.get_board();
+    int N=grid.size();
+    for (int row = 0; row < N; row++){
+      for (int col = 0; col < N; col++){
+         if(col == 3 || col == 6)
+            cout << " | ";
+         cout << grid[row][col] <<" ";
+      }
+      if(row == 2 || row == 5){
+         cout << endl;
+         for(int i = 0; i<N; i++)
+            cout << "---";
+      }
+      cout << endl;
+   }
+
+}
     for (auto row : sudoku.get_board())
     {
         for (auto val : row)
