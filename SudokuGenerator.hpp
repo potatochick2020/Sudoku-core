21fe8db42ddee49e11072488cbb9f9f86054f082
NOK <92591333+potatochick2020@users.noreply.github.com>
2023-01-14 14:57:53 +0000

    Revert "update sudokugenerator class"
    This reverts commit 8f3cbebbe86bb341273dc7296db814c245c4b694.

--- SudokuGenerator.hpp
+++ SudokuGenerator.hpp
@@ -18,23 +18,3 @@ void multi_thread_sudoku_generator_using_INT();
 void generateSudokuAndInsertTask_use_ATOMIC( DatabaseConnection DB,atomic<int> &successTask,atomic<int> &failTask, int number_of_sudoku);
 void generateSudokuAndInsertTask_use_MUTEX( DatabaseConnection DB, std::mutex &successTask_mutex, std::mutex &failTask_mutex, int &successTask, int &failTask, int number_of_sudoku);
 void generateSudokuAndInsertTask_use_INT(int x,DatabaseConnection DB,vector<int> &successTask,vector<int> &failTask, int number_of_sudoku);
-
-Class SudokuGenerator {  
-    virtual void generate();
-}
-
-Class SudokuGenerator_SingleThread : SudokuGenerator { 
-    void generate() override;
-}
-
-Class SudokuGenerator_SingleThread : SudokuGenerator { 
-    void generate() override;
-}
-
-Class SudokuGenerator_SingleThread : SudokuGenerator { 
-    void generate() override;
-}
-
-Class SudokuGenerator_SingleThread : SudokuGenerator { 
-    void generate() override;
-}
